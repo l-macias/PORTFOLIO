94b427b29b48378440aaec91b3ba2ec1abdf144f
Lucas Macias <chueko@gmail.com>
2023-05-15 13:10:16 -0300

    feat: add new project

--- l-macias-portfolio/src/App.jsx
+++ l-macias-portfolio/src/App.jsx
@@ -1,14 +1,14 @@
-import logo from './logo.svg'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { NavBar } from './components/NavBar'
-import { Banner } from './components/Banner'
-import { Skills } from './components/Skills'
-import { Projects } from './components/Projects'
-import { Contact } from './components/Contact'
-import { Footer } from './components/Footer'
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
-function App () {
+function App() {
   return (
     <div className="App">
       <NavBar />
@@ -18,7 +18,7 @@ function App () {
       <Contact />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- l-macias-portfolio/src/constants/constants.js
+++ l-macias-portfolio/src/constants/constants.js
@@ -1,79 +1,89 @@
 // Frontend Skills
 export const frontEndSkills = [
   {
-    title: 'HTML5',
-    img: './src/assets/img/skills/html5.svg'
+    title: "HTML5",
+    img: "./src/assets/img/skills/html5.svg",
   },
 
   {
-    title: 'CSS3',
-    img: './src/assets/img/skills/css3.svg'
+    title: "CSS3",
+    img: "./src/assets/img/skills/css3.svg",
   },
 
   {
-    title: 'JavaScript ES6',
-    img: './src/assets/img/skills/javascript.svg'
+    title: "JavaScript ES6",
+    img: "./src/assets/img/skills/javascript.svg",
   },
 
   {
-    title: 'React',
-    img: './src/assets/img/skills/react.svg'
+    title: "React",
+    img: "./src/assets/img/skills/react.svg",
   },
 
   {
-    title: 'Bootstrap',
-    img: './src/assets/img/skills/bootstrap.svg'
-  }
-]
+    title: "Bootstrap",
+    img: "./src/assets/img/skills/bootstrap.svg",
+  },
+];
 
 // Backend Skills
 
 export const backEndSkills = [
   {
-    title: 'Node.JS',
-    img: './src/assets/img/skills/node-js.svg'
+    title: "Node.JS",
+    img: "./src/assets/img/skills/node-js.svg",
   },
 
   {
-    title: 'Express',
-    img: './src/assets/img/skills/express.svg'
+    title: "Express",
+    img: "./src/assets/img/skills/express.svg",
   },
 
   {
-    title: 'MongoDB',
-    img: './src/assets/img/skills/mongodb.svg'
+    title: "MongoDB",
+    img: "./src/assets/img/skills/mongodb.svg",
   },
 
   {
-    title: 'MySql',
-    img: './src/assets/img/skills/mysql.svg'
-  }
-]
+    title: "MySql",
+    img: "./src/assets/img/skills/mysql.svg",
+  },
+];
 
 // Project list
 
 export const projects = [
   {
-    title: 'Blog Multiuser',
-    description: 'Admin and User panel, WYSIWYG Editor, Tags, Categories. Made with NextJS, Node.JS, Express, using MongoDB, JWT, Google Identity. Frontend Deployed on Vercel, Backend on Render',
-    imgUrl: './src/assets/img/projects/blog-multiuser.png',
-    link: 'https://frontend-blog-ten.vercel.app/',
-    github: 'https://github.com/l-macias/blog-node-mongo-react-nextjs'
+    title: "Blog Multiuser",
+    description:
+      "Admin and User panel, WYSIWYG Editor, Tags, Categories. Made with NextJS, Node.JS, Express, using MongoDB, JWT, Google Identity. Frontend Deployed on Vercel, Backend on Render",
+    imgUrl: "./src/assets/img/projects/blog-multiuser.png",
+    link: "https://frontend-blog-ten.vercel.app/",
+    github: "https://github.com/l-macias/blog-node-mongo-react-nextjs",
   },
   {
-    title: 'Managment System',
-    description: 'Products management system, with complete CRUD made with JS Vanilla, using localStorage as DB.',
-    imgUrl: './src/assets/img/projects/management-system.png',
+    title: "Managment System",
+    description:
+      "Products management system, with complete CRUD made with JS Vanilla, using localStorage as DB.",
+    imgUrl: "./src/assets/img/projects/management-system.png",
 
-    link: 'https://l-macias.github.io/gestion-final-curso-js/',
-    github: 'https://github.com/l-macias/gestion-final-curso-js'
+    link: "https://l-macias.github.io/gestion-final-curso-js/",
+    github: "https://github.com/l-macias/gestion-final-curso-js",
   },
   {
-    title: 'this.Portfolio',
-    description: 'This Portfolio was made with ReactJS, Bootstrap, Nodemailer. Deployed in a VPS using Cloudflare and Backend on Render',
-    imgUrl: './src/assets/img/projects/portfolio.png',
-    link: '#',
-    github: 'https://github.com/l-macias'
-  }
-
-]
+    title: "this.Portfolio",
+    description:
+      "This Portfolio was made with ReactJS, Bootstrap, Nodemailer. Deployed in a VPS using Cloudflare and Backend on Render",
+    imgUrl: "./src/assets/img/projects/portfolio.png",
+    link: "#",
+    github: "https://github.com/l-macias/PORTFOLIO",
+  },
+  {
+    title: "ExerciFY",
+    description:
+      "Backend of a Workout Tracker made with Node.JS, oauth, JWT, postgreSQL, Sequelize, Swagger. Deployed in Netlify and Backend on Render",
+    imgUrl: "./src/assets/img/projects/exercify.png",
+    link: "https://exercify.netlify.app/",
+    github: "https://github.com/No-Country/c10-40-ft-mern",
+  },
+];
